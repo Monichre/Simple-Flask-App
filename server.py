1d47e5a64e83d93e47b8195197f3aa8b1e56439e
Liam Ellis <liamhellis@gmail.com>
2017-05-27 16:08:44 -0400

    routing, templating and variable access

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask import render_template
+# from flask import request # -- We can also lose this as well
+
 
 app = Flask(__name__)
 
+
+# -- Here we are adding routes to a function we've called index -- literally only because its the landing page -- It's actually a view
+# -- You can add multiple routes to a function so that your application becomes more complex and so the function performs consistently
+
 @app.route('/')
-def index():
-    return   "Hello World"
+@app.route('/<name>') # -- This line tells flask to capture anything that comes after the initial forward slash -- in this case 'name'
+def index(name):
+    # name = request.args.get('name', name) # -- Therefore we can actually get rid of this line to clean up the code
+    return   render_template("name.html", name=name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    # return '{} + {} = {}'.format(num1, num2, num1 * num2)
+    return render_template("add.html", num1 = num1, num2 = num2)
 
 @app.route('/about')
 def about():
